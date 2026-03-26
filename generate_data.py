fc3dd0479d2301b8acbf95543243ff1098e3065d
Akash-ML <akash2822005@gmail.com>
2026-03-26 22:38:50 +0530

    Modified the dataset to simulate more realistic values for features.

--- generate_data.py
+++ generate_data.py
@@ -3,11 +3,26 @@ import pandas as pd
 
 data = []
 
-for _ in range(2000):
+def generate_price():
+    price = random.random()
+
+    # Chances - Low tier 35%, Mid tier 35%, High tier 30%
+    if price < 0.35:
+        return round(random.uniform(1, 100), 2)
+    elif price < 0.70:
+        return round(random.uniform(100, 1000), 2)
+    else:
+        return round(random.uniform(1000, 5000), 2)
+
+for _ in range(5000):
     quantity = random.randint(0, 100)
-    avg_daily_sales = random.randint(1, 20)
     days_to_restock = random.choice([3, 5, 7])
-    price = random.randint(1, 500) * random.choice([1, 10, 100])
+    price = generate_price()
+
+    if random.random() < 0.3:  # 30% chance of value bet. 0 and 1
+        avg_daily_sales = round(random.uniform(0, 1), 2)
+    else:
+        avg_daily_sales = round(random.uniform(1, 20), 2)
 
     low_stock = int(quantity < avg_daily_sales * days_to_restock)
 
